95231d3479872525bb82912c4ed0c126673181d6
Alex Hughes <alexhugh121@gmail.com>
2019-03-22 14:38:11 -0700

    C.Booleans

---
+++ js/app.js
@@ -0,0 +1,32 @@
+////////////////////// B. STRINGS /////////////////////////
+
+var firstVariable = "Hello World";
+firstVariable = 1;
+var secondVariable = firstVariable;
+console.log(secondVariable);
+
+var yourName = "Alex Hughes";
+
+var concantate = "Hello, my name is " + yourName;
+console.log(concantate);
+
+////////////////////// C. BOOLEANS /////////////////////////
+
+
+
+  const a = 4;
+  const b = 53;
+  const c = 57;
+  const d = 16;
+  const e = 'Kevin';
+
+  console.log(a < b);
+  console.log(c > d);
+  console.log('Name'==='Name');
+  console.log(true != false);
+  console.log(false === false === false === false === false === false === true);
+  console.log(false === false)
+  console.log(e === 'Kevin');
+  console.log(a < b < c);
+  console.log((a === a) < d);
+  console.log(48 == '48');
