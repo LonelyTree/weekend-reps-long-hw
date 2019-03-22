984c1020627c4abae49a07e65b80630c1f2b5c46
Alex Hughes <alexhugh121@gmail.com>
2019-03-22 16:04:36 -0700

    H. What's In Your Closet?

--- js/app.js
+++ js/app.js
@@ -199,6 +199,7 @@ if(incInt < 100){
 
 */
 ////////////////////// G. MONKEY IN THE MIDDLE /////////////////////////
+/*
 
 function monkeyInTheMiddle(x){
     if(x < 5){console.log("little number")}
@@ -207,3 +208,49 @@ function monkeyInTheMiddle(x){
 }
 
 monkeyInTheMiddle(11)
+
+*/
+////////////////////// H. WHAT'S IN YOUR CLOSET? /////////////////////////
+
+const kristynsCloset = [
+    'left shoe',
+    'cowboy boots',
+    'right sock',
+    'GA hoodie',
+    'green pants',
+    'yellow knit hat',
+    'marshmallow peeps'
+  ];
+  
+  // Thom's closet is more complicated. Check out this nested data structure!!
+  const thomsCloset = [
+    [
+      // These are Thom's shirts
+      'grey button-up',
+      'dark grey button-up',
+      'light blue button-up',
+      'blue button-up'
+    ],
+    [
+      // These are Thom's pants
+      'grey jeans',
+      'jeans',
+      'PJs'
+    ],
+    [
+      // Thom's accessories
+      'wool mittens',
+      'wool scarf',
+      'raybans'
+    ]
+  ];
+
+console.log(`Kristyn is rocking that ${kristynsCloset[2]} today!`);
+kristynsCloset.splice(6,0,"raybans");
+kristynsCloset[5]="stained knit hat";
+
+thomsCloset[0][0];
+thomsCloset[1][0];
+thomsCloset[2][2];
+console.log(`Thom loves repeating himself especialy with this horrible exercise where I have to type "Kristyn"(worlds most annoying name to type) multiple times. But I gues Thom is wearing a ${thomsCloset[0][0]} with some ${thomsCloset[1][1]} and ${thomsCloset[2][2]}.`);
+thomsCloset[1][2]="Footie Pajamas";
